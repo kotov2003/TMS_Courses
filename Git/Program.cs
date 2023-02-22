8a27aeb3ba96f08f1288e4dd6cc6c7183a4887d4
Alexei Voitsyk <kotov2003@yahoo.com>
2023-02-22 23:30:48 +0100

    Fixed according to pull request comments. Added parentheses. String changed to string.

--- Git/Program.cs
+++ Git/Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Enter your name:");
-        string? name = Console.ReadLine();
+        string name = Console.ReadLine();
         Console.WriteLine($"Hello {name}!\n");
 
         Task1();
@@ -14,7 +14,7 @@
 
     private static void Task1()
     {
-        Console.WriteLine($"\n{new String('=', 55)}\nWelcome to Task1!\n{new String('=', 55)}");
+        Console.WriteLine($"\n{new string('=', 55)}\nWelcome to Task1!\n{new string('=', 55)}");
 
         Console.WriteLine("Enter the first operand: ");
         string? firstOperand = Console.ReadLine();
@@ -44,12 +44,14 @@
             case "/":
                 if (operand2 == 0)
                 {
-                    Console.WriteLine($"{new String('-', 55)}\nInvalid input data: division by zero is not allowed.\n{new String('-', 55)}\n");
+                    Console.WriteLine($"{new string('-', 55)}\nInvalid input data: division by zero is not allowed.\n{new string('-', 55)}\n");
                     IsInputDataError = true;
                     break;
                 }
                 else
+                {
                     result = operand1 / operand2;
+                }
                 break;
             case "%":
                 result = operand1 % operand2;
@@ -58,32 +60,42 @@
                 break;
         }
 
-        if (!IsInputDataError) Console.WriteLine($"{new String('-', 55)}\nResult: {result}\n{new String('-', 55)}\n");
+        if (!IsInputDataError) Console.WriteLine($"{new string('-', 55)}\nResult: {result}\n{new string('-', 55)}\n");
     }
 
     private static void Task2()
     {
-        Console.WriteLine($"\n{new String('=', 55)}\nWelcome to Task2!\n{new String('=', 55)}");
+        Console.WriteLine($"\n{new string('=', 55)}\nWelcome to Task2!\n{new string('=', 55)}");
 
         Console.WriteLine("Enter any numnber between 0 and 100: ");
         int guessNumber = Convert.ToInt32(Console.ReadLine());
 
         if (51 <= guessNumber && guessNumber <= 100)
-            Console.WriteLine($"{new String('-', 55)}\nResult: the entered number belongs to [51 - 100]\n{new String('-', 55)}\n");
+        {
+            Console.WriteLine($"{new string('-', 55)}\nResult: the entered number belongs to [51 - 100]\n{new string('-', 55)}\n");
+        }
         else if (36 <= guessNumber && guessNumber <= 50)
-            Console.WriteLine($"{new String('-', 55)}\nResult: the entered number belongs to [36 - 50]\n{new String('-', 55)}\n");
+        {
+            Console.WriteLine($"{new string('-', 55)}\nResult: the entered number belongs to [36 - 50]\n{new string('-', 55)}\n");
+        }
         else if (15 <= guessNumber && guessNumber <= 35)
-            Console.WriteLine($"{new String('-', 55)}\nResult: the entered number belongs to [15 - 35]\n{new String('-', 55)}\n");
+        {
+            Console.WriteLine($"{new string('-', 55)}\nResult: the entered number belongs to [15 - 35]\n{new string('-', 55)}\n");
+        }
         else if (0 <= guessNumber && guessNumber <= 14)
-            Console.WriteLine($"{new String('-', 55)}\nResult: the entered number belongs to [0 - 14]\n{new String('-', 55)}\n");
+        {
+            Console.WriteLine($"{new string('-', 55)}\nResult: the entered number belongs to [0 - 14]\n{new string('-', 55)}\n");
+        }
         else
-            Console.WriteLine($"{new String('-', 55)}\nResult: the entered number does not belong to any of the number ranges: [0 - 14] [15 - 35] [36 - 50][50 - 100]\n{new String('-', 55)}\n");
+        {
+            Console.WriteLine($"{new string('-', 55)}\nResult: the entered number does not belong to any of the number ranges: [0 - 14] [15 - 35] [36 - 50][50 - 100]\n{new string('-', 55)}\n");
+        }
 
     }
 
     private static void Task3()
     {
-        Console.WriteLine($"\n{new String('=', 55)}\nWelcome to Task3!\n{new String('=', 55)}");
+        Console.WriteLine($"\n{new string('=', 55)}\nWelcome to Task3!\n{new string('=', 55)}");
         Console.WriteLine("Enter any word from the \"Weather\" topic: ");
 
         string? searchWord = Console.ReadLine();
@@ -123,17 +135,17 @@
                 searchResult = "fog";
                 break;
             default:
-                Console.WriteLine($"{new String('-', 55)}\nResult: no translation for this word\n{new String('-', 55)}\n");
+                Console.WriteLine($"{new string('-', 55)}\nResult: no translation for this word\n{new string('-', 55)}\n");
                 isNothingFound = true;
                 break;
         }
 
-        if (!isNothingFound) Console.WriteLine($"{new String('-', 55)}\nResult: {searchResult}\n{new String('-', 55)}\n");
+        if (!isNothingFound) Console.WriteLine($"{new string('-', 55)}\nResult: {searchResult}\n{new string('-', 55)}\n");
     }
 
     private static void Task4()
     {
-        Console.WriteLine($"\n{new String('=', 55)}\nWelcome to Task4!\n{new String('=', 55)}");
+        Console.WriteLine($"\n{new string('=', 55)}\nWelcome to Task4!\n{new string('=', 55)}");
         Console.WriteLine("Enter any number for even-odd testing:");
 
         int numberTocheck = Convert.ToInt32(Console.ReadLine());
