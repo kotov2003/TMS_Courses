6ed6aa8d41ba8b7f32e2ec0bad3edec2194af1e1
Alexei Voitsyk <kotov2003@yahoo.com>
2023-03-02 21:03:24 +0100

    Task #5_2 Task_DocumentNumber_2 is implemented

--- Git/hm4/DocumentNumberHandler.cs
+++ Git/hm4/DocumentNumberHandler.cs
@@ -11,16 +11,36 @@ namespace Git.hm4
         static public void Task_DocumentNumber_1()
         {
             Console.WriteLine("Enter document number in the following format \"xxxx-yyy-xxxx-yyy-xyxy\", where x is a number and y is a letter): ");
-
             string? documentNumber = Console.ReadLine();
 
             var result = documentNumber.Split('-');
-
 
             Console.WriteLine($"\n{new string('=', 55)}\n#1 Welcome to \'Task_DocumentNumber_1!\'\n{new string('=', 55)}");
             Console.WriteLine($"TASKS:\nInitial number:{documentNumber}\n");
             Console.WriteLine($"RESULT:");
             Console.WriteLine($"{result[0]}{result[2]}");
+            Console.WriteLine($"{new string('=', 55)}\n\n\n");
+        }
+
+        static public void Task_DocumentNumber_2()
+        {
+            Console.WriteLine("Enter document number in the following format \"xxxx-yyy-xxxx-yyy-xyxy\", where x is a number and y is a letter): ");
+            string? documentNumber = Console.ReadLine();
+
+            var documentNumberArray = documentNumber.ToCharArray();
+
+            for (int i = 0; i < documentNumberArray.Length; i++)
+            {
+                if (char.IsLetter(documentNumberArray[i]))
+                {
+                    documentNumberArray[i] = '*';
+                }
+            }
+
+            Console.WriteLine($"\n{new string('=', 55)}\n#1 Welcome to \'Task_DocumentNumber_2!\'\n{new string('=', 55)}");
+            Console.WriteLine($"TASKS:\nInitial number:{documentNumber}\n");
+            Console.WriteLine($"RESULT:");
+            Console.WriteLine($"{new string(documentNumberArray)}");
             Console.WriteLine($"{new string('=', 55)}\n\n\n");
         }
     }
