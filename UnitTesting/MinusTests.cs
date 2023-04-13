3e13b5ff07528a32bb9d64c8d176d212654c46f8
Alexei Voitsyk <kotov2003@yahoo.com>
2023-04-13 23:33:16 +0200

    Minus and Multiply tests added

--- UnitTesting/MinusTests.cs
+++ UnitTesting/MinusTests.cs
@@ -1,10 +1,50 @@
 ﻿namespace UnitTesting
 {
+    [TestFixture, Description("Minus tests")]
     internal class MinusTests
     {
+        private Calculator calculator;
+
         [SetUp]
         public void SetUp()
         {
+            calculator = new Calculator();
+        }
+
+        [TestCase(1, 2, ExpectedResult = -1), Description("Positive tests")]
+        [TestCase(-1, 2, ExpectedResult = -3)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [Category("Minus")]
+        public int MinusPositiveTest(int operand_1, int operand_2)
+        {
+            return calculator.Minus(operand_1, operand_2);
+        }
+
+        [TestCase(3, 2, 2), Description("Negative tests")]
+        [TestCase(-1, -2, -3)]
+        [TestCase(0, 0, 1)]
+        [Category("Minus")]
+        public void MinusNegativeTest(int operand_1, int operand_2, int expectedResult)
+        {
+            var actualResult = calculator.Minus(operand_1, operand_2);
+            Assert.That(actualResult, Is.Not.EqualTo(expectedResult), $"Minus {operand_1} - {operand_2} = {actualResult}");
+        }
+
+        [Test]
+        [Category("Minus"), Description("Range tests")]
+        public void MinusRangeTest([Values(1, 2, 3)] int operand_1, [Range(4, 10, 2)] int operand_2)
+        {
+            var actualResult = calculator.Minus(operand_1, operand_2);
+            Assert.That(actualResult, Is.Not.EqualTo(0), $"Minus {operand_1} - {operand_2} = {actualResult}");
+        }
+
+        [Retry(2)]
+        [Test]
+        [Category("Minus"), Description("Random tests")]
+        public void MinusRandomTest([Values(1, 2, 3)] int operand_1, [Random(1, 100, 2)] int operand_2)
+        {
+            var actualResult = calculator.Minus(operand_1, operand_2);
+            Assert.That(actualResult, Is.Not.EqualTo(0), $"Minus {operand_1} + {operand_2} = {actualResult}");
         }
 
         [TearDown]
--- UnitTesting/SummTests.cs
+++ UnitTesting/SummTests.cs
@@ -3,17 +3,15 @@
     [TestFixture, Description("Summ tests")]
     public class SummTests
     {
-        private int randomInt;
         private Calculator calculator;
 
         [SetUp]
         public void SetUp()
         {
-            randomInt = new Random().Next(0, 4);
             calculator = new Calculator();
         }
 
-        [TestCase(1, 2, ExpectedResult = 3), Description("Positive numbers tests")]
+        [TestCase(1, 2, ExpectedResult = 3), Description("Positive tests")]
         [TestCase(-1, 2, ExpectedResult = 1)]
         [TestCase(0, 0, ExpectedResult = 0)]
         [Category("Summ")]
@@ -22,7 +20,7 @@
             return calculator.Summ(operand_1, operand_2);
         }
 
-        [TestCase(1, 2, 4), Description("Negative numbers tests")]
+        [TestCase(1, 2, 4), Description("Negative tests")]
         [TestCase(-1, 2, 2)]
         [TestCase(0, 0, 9)]
         [Category("Summ")]
@@ -33,15 +31,16 @@
         }
 
         [Test]
-        [Category("Summ")]
+        [Category("Summ"), Description("Range tests")]
         public void SummRangeTest([Values(1, 2, 3)] int operand_1, [Range(1, 10, 2)] int operand_2)
         {
             var actualResult = calculator.Summ(operand_1, operand_2);
             Assert.That(actualResult, Is.Not.EqualTo(0), $"Summ {operand_1} + {operand_2} = {actualResult}");
         }
 
+        [Retry(2)]
         [Test]
-        [Category("Summ")]
+        [Category("Summ"), Description("Random tests")]
         public void SummRandomTest([Values(1, 2, 3)] int operand_1, [Random(1, 100, 2)] int operand_2)
         {
             var actualResult = calculator.Summ(operand_1, operand_2);
