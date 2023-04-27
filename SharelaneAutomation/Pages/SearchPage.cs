b4bac7202bd38c99fd07788af113f14b2047efac
Alexei Voitsyk <kotov2003@yahoo.com>
2023-04-28 00:46:34 +0200

    SearchBookTest added

--- SharelaneAutomation/Pages/SearchPage.cs
+++ SharelaneAutomation/Pages/SearchPage.cs
@@ -6,6 +6,7 @@ namespace SharelaneAutomation.Pages
     {
         By searchTitleLocator = By.XPath($"//p[.='Search']");
         By addToCartButtonLocator = By.XPath($"//img[@src='../images/add_to_cart.gif']");
+        By bookCover = By.XPath($"//img[@src='../images/product_8_large.jpg']");
 
         public SearchPage(WebDriver driver) : base(driver)
         {
@@ -21,6 +22,29 @@ namespace SharelaneAutomation.Pages
         {
             return ChromeDriver.FindElement(By.XPath($"//p[.='{keyword} ']")).Displayed;
         }
+
+        public bool CheckAuthorPresented(string author)
+        {
+            return ChromeDriver.FindElement(By.XPath($"//p[.='{author}']")).Displayed;
+        }
+
+        public bool CheckTitlePresented(string title)
+        {
+            return ChromeDriver.FindElement(By.XPath($"//p[.='{title} ']")).Displayed;
+        }
+
+        public bool CheckPricePresented(string price)
+        {
+            return ChromeDriver.FindElement(By.XPath($"//p[.='Price: ${price}']")).Displayed;
+        }
+
+        public bool CheckBookCoverPresented()
+        {
+            return ChromeDriver.FindElement(bookCover).Displayed;
+        }
+
+        //<img src="../images/product_8_large.jpg">
+
 
         public AddToCartPage ClickAddToCard()
         {
---
+++ SharelaneAutomation/Tests/SearchTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using SharelaneAutomation.Pages;
+
+namespace SharelaneAutomation.Tests
+{
+    [TestFixture]
+    public class SearchTests : SharelaneBaseTest
+    {
+        private MainPage mainPage;
+
+        [Test]
+        public void SearchBookTest()
+        {
+            var author = "W.Somerset Maugham";
+            var title = "The Moon and Sixpence";
+            var quantity = "1";
+            var price = "10.00";
+            var discountPct = "7";
+            var discountUsd = "0.7";
+            var totalUsd = "10.70";
+            var cardType = "MasterCard";
+            var cardNumber = "2222222222223642";
+
+
+            mainPage = StartPage.Login(Login, Password);
+            mainPage.SetKeyword(title);
+            var searchPage = mainPage.ClickSearchButton();
+
+            Assert.IsTrue(searchPage.IsProductFound(title));
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsTrue(searchPage.CheckAuthorPresented(author));
+                Assert.IsTrue(searchPage.CheckTitlePresented(title));
+                Assert.IsTrue(searchPage.CheckPricePresented(price));
+                Assert.IsTrue(searchPage.CheckBookCoverPresented());
+
+            });
+
+        }
+
+        [Ignore("bla")]
+        [Test]
+        public void SearchUserLoggedOutTest()
+        {
+            var author = "W.Somerset Maugham";
+            var title = "The Moon and Sixpence";
+            var quantity = "1";
+            var price = "10.00";
+            var discountPct = "7";
+            var discountUsd = "0.7";
+            var totalUsd = "10.70";
+
+            StartPage.SetKeyword(title);
+            var searchPage = StartPage.ClickSearchButton();
+
+            Assert.IsTrue(searchPage.IsProductFound(title));
+            var addToCartPage = searchPage.ClickAddToCard();
+
+            Assert.AreEqual("Oops, error. You must log in", addToCartPage.GetErrorMustLoginMessage().Text);
+        }
+    }
+}
